dc3c1534ed86f016d495b8e888ece3f4259fdc18
ogatasaori <daigocafe@msn.com>
2020-11-15 14:17:58 +0900

    商品出品機能再修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -36,11 +36,4 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:image, :name, :explanation, :category_id, :condition_id, :delivery_fee_id, :prefecture_id, :shipping_day_id, :price).merge(user_id: current_user.id)
   end
-
-
-  def move_to_index
-    unless user_signed_in?
-      redirect_to action: :index
-    end
-  end
 end
