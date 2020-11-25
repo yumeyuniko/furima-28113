b0e5f6e439101555eca39a122717818725b490df
ogatasaori <daigocafe@msn.com>
2020-11-25 20:17:59 +0900

    migrationファイル修正

--- db/migrate/20201120014257_create_delivery_destinations.rb
+++ db/migrate/20201120014257_create_delivery_destinations.rb
@@ -2,7 +2,7 @@ class CreateDeliveryDestinations < ActiveRecord::Migration[6.0]
   def change
     create_table   :delivery_destinations do |t|
       t.string     :postal_code,         null: false, default: ""
-      t.integer    :prefecture_id,       null: false, default: ""
+      t.integer    :prefecture_id,       null: false
       t.string     :city,                null: false, default: ""
       t.string     :house_number,        null: false, default: ""
       t.string     :building_name
