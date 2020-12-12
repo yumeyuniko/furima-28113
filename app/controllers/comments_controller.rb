e0fc15e251d65d05a260107b1b6e622479ef6b4a
ogatasaori <daigocafe@msn.com>
2020-12-12 18:36:22 +0900

    コメント機能修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,7 +3,7 @@ class CommentsController < ApplicationController
   def create
     @comment = Comment.new(comment_params)
     if @comment.save
-      ActionCable.server.broadcast 'comment_channel', content: @comment
+      ActionCable.server.broadcast 'comment_channel', content: @comment, user_name: @comment.user.nickname
     end
   end
 
