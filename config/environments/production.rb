06e06fa3493c47347d3eb2c83d3b05f20d22c17e
ogatasaori <daigocafe@msn.com>
2020-12-14 07:58:52 +0900

    お問合せフォーム修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -111,4 +111,17 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
   
   config.action_cable.allowed_request_origins = [ /http:\/\/.*/ ]
+
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                  587,
+  domain:               'gmail.com',
+  user_name:             ENV['GMAIL_ADRESS'],
+  password:              ENV['GMAIL_PASSWORD'],
+  authentication:       'plain',
+  enable_starttls_auto:  true
+}
+
 end
