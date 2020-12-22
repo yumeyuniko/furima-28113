fda184a5d3cdd1b0e05bf2dd3d6379e92fa9a02a
ogatasaori <daigocafe@msn.com>
2020-12-22 10:40:29 +0900

    Userモデル再修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   has_many :items, dependent: :destroy
   has_many :sns_credentials, dependent: :destroy
   has_many :comments, dependent: :destroy  # commentsテーブルとのアソシエーション
-  has_many :records
+  has_many :records, dependent: :destroy
 
   
   validates :nickname, presence: true
