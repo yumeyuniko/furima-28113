fd2ea45250391fdb8a38c7fd09a3aad211fb1a06
ogatasaori <daigocafe@msn.com>
2020-12-31 12:47:28 +0900

    ユーザー情報更新

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,8 @@ class ApplicationController < ActionController::Base
   private
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :birthday])
+    
+    devise_parameter_sanitizer.permit(:account_update, keys: [:nickname])
   end
 
   def basic_auth
